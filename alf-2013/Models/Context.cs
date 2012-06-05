9975e269031ef1bac9dbcf6813f836962473623a
Jonas Emdal <sirjonas@gmail.com>
2012-06-05 22:11:50 +0200

    Db-update

--- alf-2013/Models/Context.cs
+++ alf-2013/Models/Context.cs
@@ -4,20 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity.Infrastructure;
 
 namespace alf_2013.Models
 {
     public class WorkshopContext : DbContext
     {
+        public WorkshopContext() : base("WorkshopContext") { }
+
         public DbSet<Teacher> Teachers { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<IncludeMetadataConvention>();
         }
     }
 
-    public class WorkshopInitializer : DropCreateDatabaseIfModelChanges<WorkshopContext>
+    public class WorkshopInitializer : DropCreateDatabaseAlways<WorkshopContext>
     {
         protected override void Seed(WorkshopContext context)
         {
